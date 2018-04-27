97661f797bc04bd09c3ad39a7a7c522bac3ebaaa
krzysztof.kroczak <krzysztof.kroczak@red-gate.com>
2018-04-27 16:22:03 +0100

    Extract helper methods

---
+++ Rapson/MyMath.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Rapson
+{
+    public static class MyMath
+    {
+        public static double FindTheBestApproximation(double eps, IEnumerable<double> approximations, double startApproximation)
+        {
+            var currentApproximation = startApproximation;
+            var approximationEnumerator = approximations.GetEnumerator();
+            approximationEnumerator.MoveNext();
+            var nextApproximation = approximationEnumerator.Current;
+            while (ShouldSearchBetterApproximation(eps, nextApproximation, currentApproximation))
+            {
+
+                currentApproximation = approximationEnumerator.Current;
+                approximationEnumerator.MoveNext();
+                nextApproximation = approximationEnumerator.Current;
+            }
+
+            return nextApproximation;
+        }
+
+        public static IEnumerable<double> GenerateApproximations(Func<double, double> getNextIteration, double currentApproximation)
+        {
+            var nextApproximation = getNextIteration(currentApproximation);
+            yield return nextApproximation;
+            foreach (var approximation in GenerateApproximations(getNextIteration, nextApproximation))
+            {
+                yield return approximation;
+            }
+        }
+
+        public static bool ShouldSearchBetterApproximation(double eps, double nextApproximation, double currentApproximation)
+        {
+            return Math.Abs(nextApproximation - currentApproximation) >= eps;
+        }
+    }
+}
--- Rapson/Program.cs
+++ Rapson/Program.cs
@@ -17,14 +17,14 @@ namespace Rapson
         public static double Square(double number, double startApproximation, double eps)
         {
             double GetNextApproximationForNumber(double startApprox) => GetNextApproximation(number, startApprox);
-            var approximations = GenerateApproximations(GetNextApproximationForNumber, startApproximation);
-            return FindTheBestApproximation(eps, approximations, startApproximation);
+            var approximations = MyMath.GenerateApproximations(GetNextApproximationForNumber, startApproximation);
+            return MyMath.FindTheBestApproximation(eps, approximations, startApproximation);
         }
 
         public static double Derivative(Func<double, double> function, double number, double h, double eps)
         {
             var approximations = GenerateDerivativeApproximations(h, function, number);
-            return FindTheBestApproximation(eps, approximations, h);
+            return MyMath.FindTheBestApproximation(eps, approximations, h);
         }
 
         public static double SomeFunc(double x)
@@ -44,39 +44,7 @@ namespace Rapson
 
         public static IEnumerable<double> GenerateDerivativeApproximations(double h0, Func<double, double> function, double number)
         {
-            return GenerateApproximations(Halve, h0).Select(h => EasyDiff(function, number, h));
-        }
-
-        private static double FindTheBestApproximation(double eps, IEnumerable<double> approximations, double startApproximation)
-        {
-            var currentApproximation = startApproximation;
-            var approximationEnumerator = approximations.GetEnumerator();
-            approximationEnumerator.MoveNext();
-            var nextApproximation = approximationEnumerator.Current;
-            while (ShouldSearchBetterApproximation(eps, nextApproximation, currentApproximation))
-            {
-
-                currentApproximation = approximationEnumerator.Current;
-                approximationEnumerator.MoveNext();
-                nextApproximation = approximationEnumerator.Current;
-            }
-
-            return nextApproximation;
-        }
-
-        private static IEnumerable<double> GenerateApproximations(Func<double, double> getNextIteration, double currentApproximation)
-        {
-            var nextApproximation = getNextIteration(currentApproximation);
-            yield return nextApproximation;
-            foreach (var approximation in GenerateApproximations(getNextIteration, nextApproximation))
-            {
-                yield return approximation;
-            }
-        }
-
-        private static bool ShouldSearchBetterApproximation(double eps, double nextApproximation, double currentApproximation)
-        {
-            return Math.Abs(nextApproximation - currentApproximation) >= eps;
+            return MyMath.GenerateApproximations(Halve, h0).Select(h => EasyDiff(function, number, h));
         }
 
         private static double GetNextApproximation(double number, double currentApproximation)
