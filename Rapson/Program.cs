314e60052d28debd62ddb55de7f635323a3a6dd2
krzysztof.kroczak <krzysztof.kroczak@red-gate.com>
2018-04-27 15:10:00 +0100

    first implementation

---
+++ Rapson/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Rapson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = 567.0;
+            Console.WriteLine(Square(number, number/2, 0.00001));
+            Console.ReadKey();
+        }
+
+        public static double Square(double number, double startApproximation, double eps)
+        {
+            double nextApproximation = 0;
+            double currentApproximation = 0;
+            do
+            {
+                currentApproximation = startApproximation;
+                nextApproximation = (currentApproximation + number / currentApproximation) / 2.0;
+                startApproximation = nextApproximation;
+            } while (Math.Abs(nextApproximation - currentApproximation) >= eps);
+
+            return nextApproximation;
+        }
+    }
+}
